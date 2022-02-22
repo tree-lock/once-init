75a4ce57531a46532b3fd0622248fe69e570b825
Xmo <18851989097@163.com>
2022-02-22 10:35:02 +0800

    fix: 🐛 重构example，不再使用vanilla，过于麻烦

--- example/src/mock.ts
+++
@@ -1,5 +0,0 @@
-import { setup, mock, Random } from "mockjs";
-setup({
-  timeout: "1200-1600",
-});
-mock("/example", "get", () => Random.int(0, 10000));
--- example/vite.config.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  base: process.env.NODE_ENV === "production" ? "/once-init/" : "/",
-};
